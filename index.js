c3ddcc7f62f49c2851736322f694be1f1520a022
Harish <harishanchu@gmail.com>
2016-06-05 18:24:27 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+/**
+ * index.js
+ *
+ * @author: Harish Anchu <harishanchu@gmail.com>
+ * @copyright Copyright (c) 2015-2016, QuorraJS.
+ * @license See LICENSE.txt
+ */
+
+var support = {
+    ServiceProvider: require('./ServiceProvider')
+};
+
+module.exports = support;
